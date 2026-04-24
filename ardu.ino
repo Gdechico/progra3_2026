03043150cef7782bed70b0712defc281de91d1ea
Soy gonza <gdechico2@gmail.com>
2026-04-24 00:06:57 -0300

    inos2

---
+++ ardu.ino
@@ -0,0 +1,13 @@
+#include <Arduino.h>    
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    Serial.println("Hello, World!");
+    delay(1000); // Wait for 1 second before printing again
+}
